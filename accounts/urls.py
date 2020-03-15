93cdaefa4efded42566a3514edd89970b587af5b
brianscan14 <brianscanlon14@gmail.com>
2020-03-15 21:35:04 +0000

    adding route to reset the password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from . import urls_reset
 from .views import all_links, logout, login, register
 
 urlpatterns = [
@@ -6,4 +7,5 @@ urlpatterns = [
     url(r'^login/$', login, name='login'),
     url(r'^register/$', register, name='register'),
     url(r'^all/links/$', all_links, name='all-links'),
+    url(r'^password-reset/', include(urls_reset)),
 ]
