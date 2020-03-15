f241ae845fdaf62dbacb83e0d92833405c1c208b
brianscan14 <brianscanlon14@gmail.com>
2020-03-15 20:29:14 +0000

    removing the ability for logged in user to access login page, and user needs to be logged in, to log out now with login required decoratoir

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from accounts.forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
 
 def all_links(request):
     return render(request,  'all-links.html')
 
 
+@login_required
 def logout(request):
     auth.logout(request)
     messages.success(request, "Successfully logged out")
@@ -14,17 +16,19 @@ def logout(request):
 
 def login(request):
     """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('home'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                     password=request.POST['password'])
-            
+            messages.success(request, "You have successfully logged in!")
 
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('home'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
