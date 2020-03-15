9586ca2db8efbadbeea0ce5b65fe03ecbe24d6c9
brianscan14 <brianscanlon14@gmail.com>
2020-03-15 15:23:55 +0000

    changing home link and adding link to redirect to the pages app's urls

--- xy_fitness/urls.py
+++ xy_fitness/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views import static
 from pages.views import index
+from pages import urls as urls_pages
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index),
+    url(r'^$', index, name='index'),
+    url(r'^page/', include(urls_pages)),
 ]
