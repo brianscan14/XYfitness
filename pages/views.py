86dd0cf8ccca8e311d7c6d77bb4dbd36a382f4ff
brianscan14 <brianscanlon14@gmail.com>
2020-03-15 15:21:57 +0000

    adding views to render the pages and get them loading correctly, to be modified

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def products(request):
+    return render(request, "products.html")
+
+
+def testimonials(request):
+    return render(request, "testimonials.html")
