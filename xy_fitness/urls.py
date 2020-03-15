ce7dd3a627690ea5643a7cef3a097e683f06f3e9
brianscan14 <brianscanlon14@gmail.com>
2020-03-15 17:20:48 +0000

    adding route linking accounts urls

--- xy_fitness/urls.py
+++ xy_fitness/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.views import static
 from pages.views import home_page
 from pages import urls as urls_pages
+from accounts import urls as urls_accounts
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home_page, name='home'),
     url(r'^page/', include(urls_pages)),
+    url(r'^account/', include(urls_accounts)),
 ]
