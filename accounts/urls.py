15ac14fd521a374b0d22d6997016a5ef145bf820
brianscan14 <brianscanlon14@gmail.com>
2020-03-15 20:52:56 +0000

    adding route to register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import all_links, logout, login
+from .views import all_links, logout, login, register
 
 urlpatterns = [
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
+    url(r'^register/$', register, name='register'),
     url(r'^all/links/$', all_links, name='all-links'),
 ]
