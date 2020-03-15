9d3955ae04ae82ef7726297749e037dfdde9bf60
brianscan14 <brianscanlon14@gmail.com>
2020-03-15 22:34:21 +0000

    correcting backends file for authenticating users based off their emails too

--- accounts/backends.py
+++ accounts/backends.py
@@ -2,44 +2,34 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 
 
-class CaseInsensitiveAuth:
-    """
-    Authenticate a of User by using a case-insensitive query to check a
-    combination of the supplied email/username and password.
-    To avoid the risk of having two users with similar usernames,
-    distinguished only by letter case (e.g. 'john' and 'John'), consider
-    updating your User model to save usernames as lower case entries to
-    the database.
-    This will ensure all usernames have unique spellings, and as a result,
-    our case insensitive query will return a single result only.
-    """
-    def authenticate(self, username_or_email=None, password=None):
+class EmailAuth:
+    """use email and password match to authenticate user"""
+
+    def authenticate(self, username=None, password=None):
         """
-        Get an instance of User using the supplied username
-        or email (case insensitive) and verify the password
+        get user from email and verify password
         """
-        # Filter all users by searching for a match by username/ email.
-        users = User.objects.filter(Q(username__iexact=username_or_email) |
-                                    Q(email__iexact=username_or_email))
-        if not users:
+
+        try:
+            user = User.objects.get(email=username)
+
+            if user.check_password(password):
+                return user
+
             return None
-
-        # Then get the first result of the query (which is your user).
-        user = users[0]
-        # If the password is correct, return user object
-        if user.check_password(password):
-            return user
-
-        return None
+        except User.DoesNotExist:
+            return None
 
     def get_user(self, user_id):
         """
-        Used by the Django authentication system to retrieve a User instance
+        Django auth system retrieves user instance
         """
         try:
             user = User.objects.get(pk=user_id)
+
             if user.is_active:
                 return user
+
             return None
         except User.DoesNotExist:
             return None
