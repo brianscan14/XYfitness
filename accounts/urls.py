78d9852dcc9fbbd22b3e339a8869f1bf19cd582f
brianscan14 <brianscanlon14@gmail.com>
2020-03-15 23:05:09 +0000

    changing name of url reset file link

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from . import urls_reset
+from accounts import url_reset
 from .views import all_links, logout, login, register
 
 urlpatterns = [
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^login/$', login, name='login'),
     url(r'^register/$', register, name='register'),
     url(r'^all/links/$', all_links, name='all-links'),
-    url(r'^password-reset/', include(urls_reset)),
+    url(r'^password-reset/', include(url_reset)),
 ]
