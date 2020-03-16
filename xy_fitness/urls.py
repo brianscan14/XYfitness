979c93b9bd7e8dff3aa7208bb95e3cfa0ee0e510
brianscan14 <brianscanlon14@gmail.com>
2020-03-16 22:46:58 +0000

    adding search route

--- xy_fitness/urls.py
+++ xy_fitness/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.views import static
 from pages.views import home_page
 from pages import urls as urls_pages
+from search import urls as urls_search
 from accounts import urls as urls_accounts
 from products import urls as urls_prods
 from cart import urls as urls_cart
@@ -13,6 +14,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home_page, name='home'),
     url(r'^products/', include(urls_prods)),
+    url(r'^search/', include(urls_search)),
     url(r'^page/', include(urls_pages)),
     url(r'^account/', include(urls_accounts)),
     url(r'^cart/', include(urls_cart)),
