7e6de06353966fd0796d7b7ac9c9ec6a8e9d5da8
brianscan14 <brianscanlon14@gmail.com>
2020-03-16 15:19:32 +0000

    adding  media route to urlks file

--- xy_fitness/urls.py
+++ xy_fitness/urls.py
@@ -5,6 +5,8 @@ from pages.views import home_page
 from pages import urls as urls_pages
 from accounts import urls as urls_accounts
 from products import urls as urls_prods
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -12,4 +14,5 @@ urlpatterns = [
     url(r'^page/', include(urls_pages)),
     url(r'^account/', include(urls_accounts)),
     url(r'^products/', include(urls_prods)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
