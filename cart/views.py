6de518a433ce6129debc2cc0014330b1bb8eb4d3
brianscan14 <brianscanlon14@gmail.com>
2020-03-16 18:45:19 +0000

    adding ability to adjust items in the cart

--- cart/views.py
+++ cart/views.py
@@ -19,8 +19,21 @@ def cart_add(request, id):
             cart[id] = cart.get(id, quantity)
 
         request.session['cart'] = cart
-        return redirect(reverse('home'))
-
+        return redirect(reverse('products'))
+        
     else:
-        return redirect(reverse('home'))
+        return redirect(reverse('products'))
 
+
+@login_required
+def change_cart(request, id):
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
