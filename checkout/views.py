1bbc1b6cdaa4d9d05de9d0841afacc471282a105
brianscan14 <brianscanlon14@gmail.com>
2020-03-16 22:20:53 +0000

    adding checkout viw  to page, which interacts with the user and returns message appropriately if they incorrectly fill out a form, correcting whitespace

--- checkout/views.py
+++ checkout/views.py
@@ -48,7 +48,7 @@ def checkout(request):
             if customer.paid:
                 messages.error(request, "You have successfully paid")
                 request.session['cart'] = {}
-                return redirect(reverse('products'))
+                return redirect(reverse('home'))
             else:
                 messages.error(request, "Unable to take payment")
         else:
