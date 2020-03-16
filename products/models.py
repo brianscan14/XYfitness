b56695daa3c0542307850af5525575352d010e87
brianscan14 <brianscanlon14@gmail.com>
2020-03-16 14:12:32 +0000

    adding model of what a product is, photo to be added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.name
