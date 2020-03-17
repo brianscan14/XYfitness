a9f17a55f5ab8463bcf4c3b0ba233117e0a11f61
brianscan14 <brianscanlon14@gmail.com>
2020-03-17 22:40:29 +0000

    to combaat public access error for s3

--- custom_storages.py
+++ custom_storages.py
@@ -4,6 +4,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+    AWS_DEFAULT_ACL = 'public-read'
 
 
 class MediaStorage(S3Boto3Storage):
