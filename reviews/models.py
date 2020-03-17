6590db112b8e1098bc2cc55fa58e54aa30a292f2
brianscan14 <brianscanlon14@gmail.com>
2020-03-17 17:14:17 +0000

    adding title to review and correcting relationship with author, oneto one so that only 1 user can add 1 review, working correctly on admin page now

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=30, default='Review Title')
+    image = models.ImageField(upload_to='images', default='User image')
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.title
