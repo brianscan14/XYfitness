f7089cf3bfaff6426e62ea88fe752932f88aeb5e
brianscan14 <brianscanlon14@gmail.com>
2020-03-17 22:43:02 +0000

    adding routes to add new url, having issue with reverse redirect pk

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import all_reviews
+from .views import ViewReview, CreateReview, ReviewListView
 
 urlpatterns = [
-    url(r'^$', all_reviews, name='all_reviews'),
+    url(r'^$', ReviewListView.as_view(), name='all_reviews'),
+    url(r'review/(?P<id>\d+)', ViewReview.as_view(), name='review'),
+    url(r'^new/', CreateReview.as_view(), name='new_review')
 ]
