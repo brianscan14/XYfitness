d065f4404e0ffc1b0c0d676ae9c3f270634e8407
brianscan14 <brianscanlon14@gmail.com>
2020-03-17 15:39:45 +0000

    testing adding profile model, leaviong off for now

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
+
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
