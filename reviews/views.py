029168b38933964957d6edd4140aba4dc7270493
brianscan14 <brianscanlon14@gmail.com>
2020-03-17 17:15:18 +0000

    adding view to return all reviews, to be modified but currently working

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Review
+
 
 # Create your views here.
+def all_reviews(request):
+    stuff = {
+        'reviews': Review.objects.all()
+    }
+    return render(request, 'reviews.html', stuff)
