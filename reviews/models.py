9715d2732e5d4a7b6dc63737b929448ef9f264a2
brianscan14 <brianscanlon14@gmail.com>
2020-03-17 22:41:21 +0000

    adding absolute url to model so site knows whereto go when a review is added

--- reviews/models.py
+++ reviews/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
 
 
-# Create your models here.
 class Review(models.Model):
     title = models.CharField(max_length=30, default='Review Title')
     image = models.ImageField(upload_to='images', default='User image')
     author = models.OneToOneField(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=250)
+    date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('review', kwargs={'pk': self.pk})
