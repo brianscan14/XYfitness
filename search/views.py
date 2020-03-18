d933f856ddb70c46baa1316f0b6a12b80ad85d59
brianscan14 <brianscanlon14@gmail.com>
2020-03-18 22:20:08 +0000

    adding view to handle searches for category, workign currently, to be modified as it will be a dropdown select value

--- search/views.py
+++ search/views.py
@@ -6,7 +6,16 @@ from django.contrib import messages
 def search(request):
     results = Product.objects.filter(name__icontains=request.GET['query'])
     if not results:
-        messages.error(request, "no results pal")
+        messages.error(request, "no results for your search")
+        return redirect(reverse('home'))
+    else:
+        return render(request, "products.html", {"products": results})
+
+
+def cat_search(request):
+    results = Product.objects.filter(category__icontains=request.GET['cats'])
+    if not results:
+        messages.error(request, "no results for your search")
         return redirect(reverse('home'))
     else:
         return render(request, "products.html", {"products": results})
