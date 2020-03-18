c61117d06117f18effa00261e55db5404b24120b
brianscan14 <brianscanlon14@gmail.com>
2020-03-18 12:52:35 +0000

    adding route for either adding a review or editing a review depending on push or pull request in the view

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,6 @@ from .views import single_review, all_reviews, new_review
 urlpatterns = [
     url(r'^$', all_reviews, name='all_reviews'),
     url(r'review/(?P<pk>\d+)', single_review, name='single_review'),
-    url(r'^new/', new_review, name='new_review')
+    url(r'^new/', new_review, name='new_review'),
+    url(r'^(?P<pk>\d+)/edit/$', new_review, name='edit_review')
 ]
