0f2d617c750ee839db3fefce001cede010220bff
brianscan14 <brianscanlon14@gmail.com>
2020-03-18 22:19:05 +0000

    adding temp search url for categorties

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import search
+from .views import search, cat_search
 
 
 urlpatterns = [
-    url(r'^$', search, name='search')
+    url(r'^$', search, name='search'),
+    url(r'^heelo/$', cat_search, name='cat_search')
 ]
