883811d27ae5e1db9d3a0ac51b8747cbb5f06448
brianscan14 <brianscanlon14@gmail.com>
2020-03-18 12:59:54 +0000

    adding view to edit or create review, it takes in the author as the logged in user and uses conditional push or pull method s to see if suer is creating or editing, working fine but need to find a way of redirecting user if they have a review already

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from .models import Review
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostReviewForm
 # from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -16,5 +17,15 @@ def single_review(request, pk):
     return render(request, 'single-review.html', {'review': review})
 
 
-def new_review(request):
-     return render(request, 'reviews.html')
+def new_review(request, pk=None):
+    review = get_object_or_404(Review, pk=pk) if pk else None
+    if request.method == "POST":
+        form = PostReviewForm(request.POST, request.FILES, instance=review)
+        form.instance.author = request.user
+        if form.is_valid():
+            review = form.save()
+            return redirect(single_review, review.pk)
+    else:
+        form = PostReviewForm(instance=review)
+        form.instance.author = request.user
+    return render(request, 'addreview.html', {'form': form})
