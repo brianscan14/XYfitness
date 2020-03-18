36a2cf19ca107b8d821fe7ea93a03208c7ab46b4
brianscan14 <brianscanlon14@gmail.com>
2020-03-18 20:20:09 +0000

    adding view to send a mial to  the site owener's email, imports send mail form django mail module using 3 fields, then redirects to home view, depending on push or pull request, pull request just loads the form

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import EmailContactForm
+from django.http import HttpResponse
+from django.core.mail import send_mail, BadHeaderError
+from django.contrib import messages
 
 
 # Create your views here.
@@ -11,4 +15,15 @@ def about(request):
 
 
 def contact(request):
-    return render(request, "contact.html")
+    if request.method == 'GET':
+        form = EmailContactForm()
+    else:
+        form = EmailContactForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            send_mail(title, message, email, ['admin@hotmail.com'])
+            messages.success(request, "Successfully sent mail")
+            return redirect('home')
+    return render(request, "contact.html", {'form': form})
