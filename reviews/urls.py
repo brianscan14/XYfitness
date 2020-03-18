c81f60be53b501ef880d5e72403b73de26014d16
brianscan14 <brianscanlon14@gmail.com>
2020-03-18 13:21:45 +0000

    adding route to delete a review

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import single_review, all_reviews, new_review
+from .views import single_review, all_reviews, new_review, delete_review
 
 urlpatterns = [
     url(r'^$', all_reviews, name='all_reviews'),
     url(r'review/(?P<pk>\d+)', single_review, name='single_review'),
     url(r'^new/', new_review, name='new_review'),
-    url(r'^(?P<pk>\d+)/edit/$', new_review, name='edit_review')
+    url(r'^(?P<pk>\d+)/edit/$', new_review, name='edit_review'),
+    url(r'^(?P<pk>\d+)/delete/$', delete_review, name='delete_review')
 ]
