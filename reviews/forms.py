926e217ca9e475b7aede86b10bb787cba6bd0bb2
brianscan14 <brianscanlon14@gmail.com>
2020-03-18 12:51:54 +0000

    form content to be filled in by the user when submittign rebview

---
+++ reviews/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Review
+
+
+class PostReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ('title', 'content', 'image', 'date')
