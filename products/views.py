e8ec0423249d1012e2f9ca6c4e899fd5916ff60b
brianscan14 <brianscanlon14@gmail.com>
2020-03-19 21:15:41 +0000

    adding pagination to products view, to see if it works correctly

--- products/views.py
+++ products/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, redirect, reverse
 from .models import Product
 from django.contrib import messages
-from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def all_prods(request):
     products = Product.objects.all().order_by('-price')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products, 1)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, "products.html", {"products": products})
 
 
