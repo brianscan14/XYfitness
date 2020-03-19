3c8cbf730d22fbf8edb638e1fb21141a1c5c1a08
brianscan14 <brianscanlon14@gmail.com>
2020-03-19 21:28:12 +0000

    removing paginator as there aren't enough products to warrant it and it looks silly to have it on the page then

--- products/views.py
+++ products/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render, redirect, reverse
 from .models import Product
 from django.contrib import messages
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 
 # Create your views here.
 def all_prods(request):
-    products = Product.objects.all().order_by('-price')
-    page = request.GET.get('page', 1)
-    paginator = Paginator(products, 1)
-    try:
-        products = paginator.page(page)
-    except PageNotAnInteger:
-        products = paginator.page(1)
-    except EmptyPage:
-        products = paginator.page(paginator.num_pages)
+    products = Product.objects.all()
     return render(request, "products.html", {"products": products})
 
 
