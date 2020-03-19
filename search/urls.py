8fb4f5c4301141e19ea69eac4d2ced8980fff4a2
brianscan14 <brianscanlon14@gmail.com>
2020-03-19 15:04:51 +0000

    adding route to search for one of either category via a link on site

--- search/urls.py
+++ search/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import search, cat_search, LtoH, sort
+from .views import search, cat_search, plans, apparel, sort
 
 
 urlpatterns = [
     url(r'^$', search, name='search'),
     url(r'^categories/$', cat_search, name='cat_search'),
-    url(r'^LtoH/$', LtoH, name='LtoH'),
+    url(r'^plans/$', plans, name='plans'),
+    url(r'^apparel/$', apparel, name='apparel'),
     url(r'^sort/$', sort, name='sort')
 ]
