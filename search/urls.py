f1d0c98360dd6d19404a25cb62aff3b5b5926b7f
brianscan14 <brianscanlon14@gmail.com>
2020-03-19 13:28:00 +0000

    adding route to sort my products

--- search/urls.py
+++ search/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import search, cat_search, LtoH
+from .views import search, cat_search, LtoH, sort
 
 
 urlpatterns = [
     url(r'^$', search, name='search'),
     url(r'^categories/$', cat_search, name='cat_search'),
-    url(r'^LtoH/$', LtoH, name='LtoH')
+    url(r'^LtoH/$', LtoH, name='LtoH'),
+    url(r'^sort/$', sort, name='sort')
 ]
