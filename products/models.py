f99585aea55a23be349520b4b964990170a5afb0
brianscan14 <brianscanlon14@gmail.com>
2020-03-20 12:31:39 +0000

    correcting default suggestion in product model

--- products/models.py
+++ products/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     image = models.ImageField(upload_to='images', default='XYfitness image')
-    name = models.CharField(max_length=100, default=' XYfitness Product')
+    name = models.CharField(max_length=100, default='XYfitness Product')
     description = models.TextField(max_length=250)
     price = models.DecimalField(max_digits=4, decimal_places=2)
-    category = models.CharField(max_length=30, default='plan')
+    category = models.CharField(max_length=30, default='plan or apparel')
 
     def __str__(self):
         return self.name
