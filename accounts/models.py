2189e23cb29d109f508f74eb368419794e4b6df2
brianscan14 <brianscanlon14@gmail.com>
2020-03-21 22:10:53 +0000

    testing adding prfile model to update account details

--- accounts/models.py
+++ accounts/models.py
@@ -1,2 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30, default='New User')
+
+    def __str__(self):
+        return self.username
