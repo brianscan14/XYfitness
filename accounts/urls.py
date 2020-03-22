ae2e7de754f1dc6ae3ee118a383cf1ff7cd28c5e
brianscan14 <brianscanlon14@gmail.com>
2020-03-22 13:16:39 +0000

    adding route to update password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from accounts import url_reset
-from .views import all_links, logout, login, register, profile, update_profile
+from .views import all_links, logout, login, register, profile, update_profile, update_password
 
 urlpatterns = [
     url(r'^logout/$', logout, name='logout'),
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^all/links/$', all_links, name='all-links'),
     url(r'^profile/update/$', update_profile, name='update_profile'),
+    url(r'^profile/update/password/$', update_password, name='update_password'),
     url(r'^password-reset/', include(url_reset)),
 ]
