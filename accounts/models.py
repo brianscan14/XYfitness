0c3051180bfe490d9b1a8041c327eff3317264c3
brianscan14 <brianscanlon14@gmail.com>
2020-03-22 20:40:15 +0000

    adding image field to profile model, to allow user to change it

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=30, default='New User')
+    username = models.CharField(max_length=30, default='User')
+    profile_pic = models.ImageField(upload_to='images', default='User Image')
 
     def __str__(self):
         return self.username
