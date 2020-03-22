eb0857da3da928747c75aad8f39e7cfaa265cdf3
brianscan14 <brianscanlon14@gmail.com>
2020-03-22 20:44:47 +0000

    adding route to allow user update their profile pic

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from accounts import url_reset
-from .views import all_links, logout, login, register, profile, update_profile, update_password
+from .views import all_links, logout, login, register, profile, update_profile, update_password, update_profile_pic
 
 urlpatterns = [
     url(r'^logout/$', logout, name='logout'),
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^all/links/$', all_links, name='all-links'),
     url(r'^profile/update/$', update_profile, name='update_profile'),
+    url(r'^profile/update/picture/$', update_profile_pic, name='update_profile_pic'),
     url(r'^profile/update/password/$', update_password, name='update_password'),
     url(r'^password-reset/', include(url_reset)),
 ]
