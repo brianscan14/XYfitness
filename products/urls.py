a7cc440b71b33a338cd47c8f74cbf3f87d7cea18
brianscan14 <brianscanlon14@gmail.com>
2020-03-23 17:22:48 +0000

    adding route to view a product by itself

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import all_prods, plans, apparel, sort, sort_apparel, sort_plans
+from .views import all_prods, plans, apparel, sort, sort_apparel, sort_plans, single_prod
 
 urlpatterns = [
     url(r'^$', all_prods, name='products'),
+    url(r'product/(?P<pk>\d+)', single_prod, name='single_prod'),
     url(r'^plans/$', plans, name='plans'),
     url(r'^apparel/$', apparel, name='apparel'),
     url(r'^sort/$', sort, name='sort'),
