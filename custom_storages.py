45a413c6479eb98e735087ad1fa54627c2965374
brianscan14 <brianscanlon14@gmail.com>
2020-03-23 15:17:30 +0000

    disabling staticfiles storage on s3 for the duration of testing as it isnt very productive to push these changes to s3 everytime one is made

--- custom_storages.py
+++ custom_storages.py
@@ -2,9 +2,8 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
-    AWS_DEFAULT_ACL = 'public-read'
+# class StaticStorage(S3Boto3Storage):
+#     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
