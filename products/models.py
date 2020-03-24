c5d501fd070f16fc0825abe84eafe3ea0d9d0ca9
brianscan14 <brianscanlon14@gmail.com>
2020-03-24 17:16:47 +0000

    making products and user fields FK ass i want many reviews per product, byut no product can have the same 2 reviews by 1 user, this will them need to be implemented in the view file

--- products/models.py
+++ products/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
 
 
 class ProductReview(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, unique=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     title = models.CharField(max_length=30, default='Review Title')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=250)
