173e8217c825cb4ec08a4bb264e70f26ae276888
brianscan14 <brianscanlon14@gmail.com>
2020-03-30 22:36:23 +0000

    adding form to select size and add to cart

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import ProductReview
+from .models import ProductReview, Product
 
 
 class ProdReviewForm(forms.ModelForm):
@@ -7,3 +7,10 @@ class ProdReviewForm(forms.ModelForm):
     class Meta:
         model = ProductReview
         fields = ('title', 'content', 'rating')
+
+
+class ProdSizeForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
