826552afbcdebeeed775580bf8141bb3159f941a
brianscan14 <brianscanlon14@gmail.com>
2020-03-31 21:51:29 +0000

    contents file going to need to be updated in order to display the quantity and sizes correctly to user, commenting out th code for now as it causes errors on main page

--- cart/contexts.py
+++ cart/contexts.py
@@ -4,15 +4,22 @@ from products.models import Product
 
 def cart_contents(request):
     cart = request.session.get('cart', {})
+    print(cart)
 
     cart_items = []
     prod_count = 0
     total = 0
 
-    for id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=id)
-        total += quantity * product.price
-        prod_count += quantity
-        cart_items.append({'id': id, 'quantity': quantity, 'product': product})
+    # for id, content in cart.items():
+    #     product = get_object_or_404(Product, pk=id)
+    #     quantity = content[0]
+    #     size = content[1]
+    #     total += quantity * product.price
+    #     # for size, quantity in content.items():
+    #     #     print(size, quantity)
+    #         # # total += quantity * product.price
+    #         # prod_count += quantity
+    #     prod_count += quantity
+    #     cart_items.append({'id': id, 'quantity': quantity, 'size': size, 'product': product})
 
     return {'cart_items': cart_items, 'total': total, 'product_count': prod_count}
