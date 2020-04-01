764369e0237c290470efd8b378e996e17d8071af
brianscan14 <brianscanlon14@gmail.com>
2020-04-01 19:04:55 +0000

    tidying up code by indenting items appended

--- cart/contexts.py
+++ cart/contexts.py
@@ -10,16 +10,19 @@ def cart_contents(request):
     prod_count = 0
     total = 0
 
-    # for id, content in cart.items():
-    #     product = get_object_or_404(Product, pk=id)
-    #     quantity = content[0]
-    #     size = content[1]
-    #     total += quantity * product.price
-    #     # for size, quantity in content.items():
-    #     #     print(size, quantity)
-    #         # # total += quantity * product.price
-    #         # prod_count += quantity
-    #     prod_count += quantity
-    #     cart_items.append({'id': id, 'quantity': quantity, 'size': size, 'product': product})
+    for id, content in cart.items():
+        product = get_object_or_404(Product, pk=id)
+        # quantity = content[]
+        # # # size = content[1]
+        # total += quantity * product.price
+        for size, quantity in content.items():
+            total += quantity * product.price
+            prod_count += quantity
+            cart_items.append({
+                'id': id, 'quantity': quantity,
+                'size': size, 'total': total, 'product': product
+            })
 
-    return {'cart_items': cart_items, 'total': total, 'product_count': prod_count}
+    return {
+        'cart_items': cart_items, 'total': total, 'product_count': prod_count
+    }
