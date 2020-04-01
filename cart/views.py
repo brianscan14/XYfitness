690ce3df002720a6a565b864411a50d2280a9b2c
brianscan14 <brianscanlon14@gmail.com>
2020-04-01 19:04:12 +0000

    correcting method for adding items to cart, now trying to correct adjust cart method

--- cart/views.py
+++ cart/views.py
@@ -28,39 +28,9 @@ def cart_add(request, id):
         cart[id] = {s: q}
         print("id not in cart")
 
-    # if id in cart:
-    #     if size == cart[id][1]:  # if size equal to size there already in cart
-    #         cart[id] = [int(cart[id][0]) + quantity, size]
-    #         # adding the quantity added to
-    #         # quantity integer there already
-    #         print(cart[id])
-    #     else:
-    #         newid = [quantity, size]
-    #         print(newid)
-    #         print(cart[id])
-    #         cart[id] = cart[id] + newid
-
-    #         # add another k/v pair to your cart id if size there
-    #         # if the size is not already in the cart then you
-    #         # want to add this new k/v pair onto this prod id
-    #         # if size = cart[id][1] else:
-    #         # print(cart[id][0])
-    #         # print(cart[id][1])
-    #         print(cart[id])
-    # else:
-    #     cart[id] = cart.get(int(id), [quantity, size])
-    #     print(cart[id])
-
-    # if id in cart:
-    #     # size = cart[id][1]
-    #     if size in cart[id]:
-    #         cart[id] = [int(cart[id][0]) + quantity, size]
-    #         print(cart[id][size])
-    #     else:
-    #         cart[id] = [int(cart[id]) + quantity, size]
-    #         # [int(cart[id][0]).update([(quantity, size)])]
-    #         # cart[id] = cart[id].update({size: quantity for size, quantity in cart[id].iteritems() if quantity})
-    #         print(cart[id])
+    # add another k/v pair to your cart id if size there
+    # if the size is not already in the cart then you
+    # want to add this new k/v pair onto this prod id
 
     request.session['cart'] = cart
     messages.success(request, "Item added to cart")
@@ -74,9 +44,19 @@ def change_cart(request, id):
     cart = request.session.get('cart', {})
 
     if quantity > 0:
-        cart[id][size] = quantity
+        if size in cart[id]:
+            cart[id][size] = quantity
+            print(cart[id])
+        else:
+            cart[id].pop(size)
+            # cart[id][size] = cart[id].pop('size', quantity)
+            # cart[id].update(cart[id])
+            # cart[id][size] = quantity
+            # print(cart[id])
     else:
-        cart.pop(id[size])
+        # cart[id].pop([id][quantity])
+        cart[id].pop([id][quantity])
+        # if else to pop out qunatity if size is 0
 
     request.session['cart'] = cart
     messages.success(request, "Cart updated")
