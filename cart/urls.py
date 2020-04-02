2309de888cb8d00da9d46b6ccd225b46e503d096
brianscan14 <brianscanlon14@gmail.com>
2020-04-02 01:02:14 +0000

    adding delete route ti urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import view_cart, cart_add, change_cart
+from .views import view_cart, cart_add, change_cart, del_cart_item
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
     url(r'^add/(?P<id>\d+)', cart_add, name='cart_add'),
     url(r'^change/(?P<id>\d+)', change_cart, name='change_cart'),
+    url(r'^delete/(?P<id>\d+)', del_cart_item, name='del_cart_item')
 ]
