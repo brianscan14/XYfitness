45d0b3b5ad3b4e20faf27c8472c6314f302677e1
brianscan14 <brianscanlon14@gmail.com>
2020-04-03 02:40:36 +0000

    adding scroll to top button for users to scroll to the top of page

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,34 @@
 console.log("Hello world!");
+
+
+/**
+ * @scrollToTop
+ * This function shows the button when the user scrolls down 80px from top of document
+ * @topPage
+ * This function scrolls to the top of the page when the button is clicked
+ */
+
+let mybutton = document.getElementById("topBtn");
+
+window.addEventListener('scroll', function(e) {
+    scrollToTop()
+});
+
+function scrollToTop() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    mybutton.style.display = "block";
+        } else {
+    mybutton.style.display = "none";
+        }
+}
+
+function topPage() {
+    document.body.scrollTo({top: 0, behavior: 'smooth'});
+    document.documentElement.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+$(document).ready(function() {
+    $('#topBtn').click(function(){
+        topPage();
+    });
+});
