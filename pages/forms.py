0eb521e7ae2556d51be2cbec80ecdc7e6ab78162
brianscan14 <brianscanlon14@gmail.com>
2020-04-05 19:33:47 +0000

    quickly editing contact page, giving it a col6 width, centering the heading, adding placeholdser to message field also, and centering the send button, still to be updated

--- pages/forms.py
+++ pages/forms.py
@@ -3,5 +3,10 @@ from django import forms
 
 class EmailContactForm(forms.Form):
     email = forms.EmailField(required=True)
-    title = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    title = forms.CharField(required=True, min_length=5, max_length=40)
+    message = forms.CharField(widget=forms.Textarea(
+        attrs={
+            'rows': '9',
+            'placeholder': 'We will reply as soon as we can :)'
+        }),
+        min_length=40, max_length=400, required=True)
