b83e5bcd867be955d53168aebe1a71be1222cc19
brianscan14 <brianscanlon14@gmail.com>
2020-04-06 20:47:14 +0000

    changing message returned for no results

--- search/views.py
+++ search/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 def search(request):
     results = Product.objects.filter(name__icontains=request.GET['query'])
     if not results:
-        messages.error(request, "no results for your search")
+        messages.error(request, "no results, see if anything matches below")
         return redirect(reverse('products'))
     else:
         return render(request, "searchresults.html", {"products": results})
