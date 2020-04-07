029325e9ba4860b9afeff2f3345c72d06640a284
brianscan14 <brianscanlon14@gmail.com>
2020-04-07 19:40:41 +0000

    adding route for pre checkout delivery page

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import checkout
+from .views import checkout, delivery
 
 urlpatterns = [
     url(r'^$', checkout, name='checkout'),
+    url(r'^deliver/$', delivery, name='delivery'),
 ]
