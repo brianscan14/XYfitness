67b29d841ced2cf98aab420f3db03f2f8b6e87b0
brianscan14 <brianscanlon14@gmail.com>
2020-04-07 12:55:27 +0000

    adding check to see if any of the items contains the apparel category field, if so then they will need to be delivered and this incurrs a delivery fee to be added to the total

--- cart/contexts.py
+++ cart/contexts.py
@@ -4,17 +4,20 @@ from products.models import Product
 
 def cart_contents(request):
     cart = request.session.get('cart', {})
-    print(cart)
-
     cart_items = []
     prod_count = 0
     total = 0
+    del_total = 0
 
     for id, content in cart.items():
         product = get_object_or_404(Product, pk=id)
-        # quantity = content[]
-        # # # size = content[1]
-        # total += quantity * product.price
+        print(product.category)
+        cat = Product.objects.filter(id=id, category__icontains='A')
+        if cat:
+            del_total = 5
+        else:
+            del_total = 0
+        print(del_total)
         for size, quantity in content.items():
             this_total = quantity * product.price
             total += quantity * product.price
@@ -24,6 +27,15 @@ def cart_contents(request):
                 'size': size, 'total': total, 'product': product
             })
 
+    subtotal = total
+    print(subtotal)
+    total = del_total + total
+    print(total)
+
     return {
-        'cart_items': cart_items, 'total': total, 'product_count': prod_count
+        'cart_items': cart_items,
+        'total': total,
+        'subtotal': subtotal,
+        'del_total': del_total,
+        'product_count': prod_count
     }
