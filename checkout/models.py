0b20184ed9cbcda1dbdcd1d1157a8e27875d2b46
brianscan14 <brianscanlon14@gmail.com>
2020-04-07 13:22:13 +0000

    adding label to street address 1 and 2 fields

--- checkout/models.py
+++ checkout/models.py
@@ -8,8 +8,12 @@ class Order(models.Model):
     country = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
-    street_address1 = models.CharField(max_length=40, blank=False)
-    street_address2 = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(
+        max_length=40, verbose_name=u'House name / apartment no.', blank=True
+        )
+    street_address2 = models.CharField(
+        max_length=40, verbose_name=u'Townland', blank=False
+        )
     county = models.CharField(max_length=40, blank=False)
     date = models.DateField()
 
