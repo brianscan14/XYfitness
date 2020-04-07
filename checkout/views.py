59cf7c9c9bad678ceff12465c99cd6cb77254202
brianscan14 <brianscanlon14@gmail.com>
2020-04-07 19:49:14 +0000

    adding view for user delivery details, form used in the checkout view will get its data from the session, working correctly

--- checkout/views.py
+++ checkout/views.py
@@ -13,9 +13,27 @@ stripe.api_key = settings.STRIPE_SECRET
 
 
 @login_required()
-def checkout(request):
-    if request.method == "POST":
+def delivery(request):
+    if request.method == 'POST':
         order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            request.session['form_data_page_1'] = order_form.cleaned_data
+            return redirect('checkout')
+    else:
+        order_form = OrderForm(request.session['form_data_page_1'])
+    return render(request, "deliver.html", {"order_form": order_form})
+
+# @login_required()
+# def verify_delivery(request, content):
+#     return render(request, 'myapp/verify_content.html', {'content' : content})
+
+
+@login_required()
+def checkout(request):
+    deliv_details = request.session['form_data_page_1']
+    order_form = OrderForm(deliv_details)
+    if request.method == "POST":
+        # order_form = request.session['order_form']
         payment_form = MakePaymentForm(request.POST)
 
         if order_form.is_valid() and payment_form.is_valid():
@@ -26,6 +44,7 @@ def checkout(request):
             cart = request.session.get('cart', {})
             total = 0
             del_total = 0
+
             for id, content in cart.items():
                 product = get_object_or_404(Product, pk=id)
                 cat = Product.objects.filter(id=id, category__icontains='A')
@@ -33,16 +52,13 @@ def checkout(request):
                     del_total = 5
                 for size, quantity in content.items():
                     total += quantity * product.price
-                    print(total)
                     order_line_item = OrderLineItem(
                         order=order,
                         product=product,
                         quantity=quantity,
                     )
                     order_line_item.save()
-                    print(order_line_item)
             total = del_total + total
-            print(total)
             try:
                 customer = stripe.Charge.create(
                     amount=int(total * 100),
@@ -64,8 +80,10 @@ def checkout(request):
             messages.error(request, "Unable to take a payment with that card!")
     else:
         payment_form = MakePaymentForm()
-        order_form = OrderForm()
+        
 
-    return render(request, "checkout.html", {"order_form": order_form,
+    return render(request, "checkout.html", {
+        "order_form": order_form,
         "payment_form": payment_form,
-        "publishable": settings.STRIPE_PUBLISHABLE})
+        "publishable": settings.STRIPE_PUBLISHABLE
+        })
