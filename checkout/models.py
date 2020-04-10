a2787e7e491d049bfd9745f8a88bbf1acd42bec7
brianscan14 <brianscanlon14@gmail.com>
2020-04-10 23:59:28 +0000

    after alot of trial and error, checkout DB has been corrected, the initial one had to be re done as  the products depended on this, all seems to be working fine now, and new heroku db also seems to be ok after testing

--- checkout/models.py
+++ checkout/models.py
@@ -4,7 +4,7 @@ from products.models import Product
 
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=20, blank=False)
+    phone_number = models.IntegerField(blank=False)
     country = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
