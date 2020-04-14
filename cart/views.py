c834d0b1e02ccc823c00541e8b2db90b3d9d433e
brianscan14 <brianscanlon14@gmail.com>
2020-04-14 16:48:14 +0000

    removing import, login not required for cart, only for  checkout

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from products.views import single_prod
 
