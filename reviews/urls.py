870b4da44b3ed7489f2e1068a250b4465d7ab4c2
brianscan14 <brianscanlon14@gmail.com>
2020-04-14 11:33:35 +0000

    changing view name from all reviews to testimonials

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import single_review, all_reviews, new_review, delete_review
 
 urlpatterns = [
-    url(r'^$', all_reviews, name='all_reviews'),
+    url(r'^$', all_reviews, name='testimonials'),
     url(r'review/(?P<pk>\d+)', single_review, name='single_review'),
     url(r'^new/', new_review, name='new_review'),
     url(r'^(?P<pk>\d+)/edit/$', new_review, name='edit_review'),
