39d0fab2b483a874bcbac67d4a842c83528fe5f2
brianscan14 <brianscanlon14@gmail.com>
2020-04-19 14:33:46 +0000

    updating search function with description

--- search/views.py
+++ search/views.py
@@ -4,6 +4,10 @@ from django.contrib import messages
 
 
 def search(request):
+    """
+    Searches product DB for string matches of names that match
+    the query inputted by the user.
+    """
     results = Product.objects.filter(name__icontains=request.GET['query'])
     if not results:
         messages.error(request, "no results, see if anything matches below")
