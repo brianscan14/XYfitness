a171257c56d9ca7110508c30dafc731809d91151
brianscan14 <brianscanlon14@gmail.com>
2020-04-19 14:30:51 +0000

    updating contact model with comment

--- pages/models.py
+++ pages/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 
-# Create your models here.
 class Query(models.Model):
+    """Content of contact from returned to admin site"""
     title = models.CharField(max_length=30, default='Query')
     email = models.CharField(max_length=30, default='User')
     message = models.TextField(max_length=250)
