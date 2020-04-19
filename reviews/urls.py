1757f33afd29a2f3e54b9e447d67c3c51bba97e9
brianscan14 <brianscanlon14@gmail.com>
2020-04-19 13:51:17 +0000

    corrected issue when testing add / eddit reviews, urls were incorrect so they werent pointing correctly to th respective forms or allwoign you to delete review, edit data pulling correctly now and delte working

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from .views import single_review, all_reviews, new_review, delete_review
 
 urlpatterns = [
-    url(r'^$', all_reviews, name='testimonials'),
-    url(r'^(?P<pk>\d+)', single_review, name='single_review'),
-    url(r'^new/', new_review, name='new_review'),
+    url(r'^all/$', all_reviews, name='testimonials'),
+    url(r'^(?P<pk>\d+)/$', single_review, name='single_review'),
+    url(r'^share/$', new_review, name='new_review'),
     url(r'^(?P<pk>\d+)/edit/$', new_review, name='edit_review'),
     url(r'^(?P<pk>\d+)/delete/$', delete_review, name='delete_review')
 ]
