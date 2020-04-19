9336913605959371793a49f04d195f7baf83df58
brianscan14 <brianscanlon14@gmail.com>
2020-04-19 14:38:31 +0000

    adding docstring for contactform

--- pages/forms.py
+++ pages/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class EmailContactForm(forms.Form):
+    """Form filled out by user to contact the PT."""
     email = forms.EmailField(required=True)
     title = forms.CharField(required=True, min_length=5, max_length=40)
     message = forms.CharField(widget=forms.Textarea(
