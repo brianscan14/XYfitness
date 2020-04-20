c0ee6d3bc1684193fca98f2a1935cd0e5d9995bb
brianscan14 <brianscanlon14@gmail.com>
2020-04-20 10:38:51 +0000

    using sweetify instead of messages for user interaction correspondence and adding / updating cart

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import messages
 from products.views import single_prod
+import sweetify
 
 
 def view_cart(request):
@@ -15,16 +15,46 @@ def cart_add(request, id):
 
     if id in cart:
         if category == 'plan':
-            messages.error(request, "Plan in cart already")
+            sweetify.success(
+                request,
+                "Plan in cart already, can't add again!",
+                icon='info',
+                timer='3000',
+                toast='true',
+                position='top-end',
+            )
         elif s in cart[id]:
             cart[id][s] = cart[id][s] + q
-            messages.success(request, "Cart updated")
+            sweetify.success(
+                request,
+                "Cart updated",
+                icon='success',
+                timer='3000',
+                toast='true',
+                position='top-end',
+            )
         else:
             cart[id][s] = q  # lhs of new dict = rhs
-            messages.success(request, "Item added to cart")
+            sweetify.success(
+                request,
+                "Item added to cart",
+                icon='success',
+                timer='1500',
+                toast='true',
+                position='center',
+                background='#181818',
+            )
     else:
         cart[id] = {s: q}
-        messages.success(request, "Item added to cart")
+        sweetify.success(
+            request,
+            "Item added to cart",
+            icon='success',
+            timer='1500',
+            toast='true',
+            position='center',
+            background='#181818',
+        )
 
     # add another k/v pair to your cart id if size there
     # if the size is not already in the cart then you
@@ -50,7 +80,14 @@ def change_cart(request, id):
         cart[id][size] = quantity
 
     request.session['cart'] = cart
-    messages.success(request, "Cart updated")
+    sweetify.success(
+        request,
+        "Item updated",
+        icon='success',
+        timer='2500',
+        toast='true',
+        position='top-end',
+    )
     return redirect(reverse('view_cart'))
 
 
@@ -65,5 +102,12 @@ def del_cart_item(request, id):
         del cart[id]
 
     request.session['cart'] = cart
-    messages.success(request, "Item removed")
+    sweetify.success(
+        request,
+        "Item removed",
+        icon='success',
+        timer='3000',
+        toast='true',
+        position='top-end',
+    )
     return redirect(reverse('view_cart'))
