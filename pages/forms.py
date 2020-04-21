2977d4df296bb3f770e4d0f6cfcaaf325f6e423f
brianscan14 <brianscanlon14@gmail.com>
2020-04-21 16:54:41 +0000

    removing duplicate max length attributes in froms file when it id in the model already, increasing max character count for message

--- pages/forms.py
+++ pages/forms.py
@@ -4,10 +4,10 @@ from django import forms
 class EmailContactForm(forms.Form):
     """Form filled out by user to contact the PT."""
     email = forms.EmailField(required=True)
-    title = forms.CharField(required=True, min_length=5, max_length=40)
+    title = forms.CharField(required=True, min_length=5)
     message = forms.CharField(widget=forms.Textarea(
         attrs={
             'rows': '9',
             'placeholder': 'We will reply as soon as we can :)'
         }),
-        min_length=40, max_length=400, required=True)
+        min_length=30, required=True)
--- pages/models.py
+++ pages/models.py
@@ -5,7 +5,7 @@ class Query(models.Model):
     """Content of contact from returned to admin site"""
     title = models.CharField(max_length=30, default='Query')
     email = models.CharField(max_length=30, default='User')
-    message = models.TextField(max_length=250)
+    message = models.TextField(max_length=400)
 
     def __str__(self):
         return self.message
