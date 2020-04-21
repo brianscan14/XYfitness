c24fd529904bc5762919450ff09086ffb9a35f84
brianscan14 <brianscanlon14@gmail.com>
2020-04-21 14:47:06 +0000

    altering default pic for testimonial for the user to be the designated default user pic

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,9 @@ from django.utils import timezone
 
 class Review(models.Model):
     title = models.CharField(max_length=30, default='Review Title')
-    image = models.ImageField(upload_to='images', default='User image')
+    image = models.ImageField(
+        upload_to='images', default='images/4648398-blue-steel.jpg'
+    )
     author = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     content = models.TextField(max_length=250)
     date = models.DateTimeField(default=timezone.now)
