fb07b3f9dfa95482cc85f9f2f29d84d9ae38681e
brianscan14 <brianscanlon14@gmail.com>
2020-04-25 15:59:35 +0000

    updating stripe function, adding callback to be used when creating token instead

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,31 +1,40 @@
 $(function() {
+
     $("#payment-form").submit(function() {
-        var form = this;
-        var card = {
+        let form = this;
+        let card = {
             number: $("#id_credit_card_number").val(),
             expMonth: $("#id_expiry_month").val(),
             expYear: $("#id_expiry_year").val(),
             cvc: $("#id_cvv").val() 
         };
-    
-    Stripe.createToken(card, function(status, response) {
-        if (status === 200) {
+
+        Stripe.createToken(card, stripeCreateHandler);
+
+        return false;
+    });
+});
+
+    function stripeCreateHandler(status, response) {
+        let form = document.getElementById('payment-form');
+
+        if (response.error) {
+
+            $("#stripe-error-message").text(response.error.message);
+            $("#credit-card-errors").show();
+            $("#validate_card_btn").attr("disabled", false);
+            
+        } else {
+            
             $("#credit-card-errors").hide();
             $("#id_stripe_id").val(response.id);
 
-            // stop card details being submitted to server
             $("#id_credit_card_number").removeAttr('name');
             $("#id_cvv").removeAttr('name');
             $("#id_expiry_month").removeAttr('name');
             $("#id_expiry_year").removeAttr('name');
 
             form.submit();
-        } else {
-            $("#stripe-error-message").text(response.error.message);
-            $("#credit-card-errors").show();
-            $("#validate_card_btn").attr("disabled", false);
         }
-    });
-    return false;
-    });
-});
+
+    };
