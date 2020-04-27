e8b805c0df3d003d14ca1371c31f4452582d9843
brianscan14 <brianscanlon14@gmail.com>
2020-04-27 14:28:51 +0000

    reverting back to s3 static for deployment

--- xy_fitness/settings.py
+++ xy_fitness/settings.py
@@ -131,7 +131,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
-# STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
